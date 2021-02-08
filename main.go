b3b2c99b39c4d7c5fdae42777fe4884709a66944
hamizan-mys <78681575+hamizan-mys@users.noreply.github.com>
2021-02-08 14:54:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - VisMark #4"}
+		res := &response{Message: "Hello World - VisMark #5"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
